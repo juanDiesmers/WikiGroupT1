41f4b13348996ab67ac5ad58c3edcbe99f6c6300
amers_gos <juandiegopala_@outlook.es>
2024-02-13 17:02:23 -0500

    entity project

---
+++ wiki/src/main/java/co/wiki/wiki/entity/Application.java
@@ -0,0 +1,35 @@
+package co.wiki.wiki.entity;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import ch.qos.logback.core.status.Status;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Where(clause = "status = 0")
+@SQLDelete(sql = "UPDATE application SET  status = 1 WHERE id=?")
+public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String url;
+    private String code;
+    private String internalCode;
+    private Status status;
+
+    
+}
