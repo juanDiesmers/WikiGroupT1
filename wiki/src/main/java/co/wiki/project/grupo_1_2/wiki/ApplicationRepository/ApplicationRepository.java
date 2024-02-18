f4803666f952bf15279cf075ac481cd8fce904f2
amers_gos <juandiegopala_@outlook.es>
2024-02-18 13:40:07 -0500

    New Project

---
+++ wiki/src/main/java/co/wiki/project/grupo_1_2/wiki/ApplicationRepository/ApplicationRepository.java
@@ -0,0 +1,11 @@
+package co.wiki.project.grupo_1_2.wiki.ApplicationRepository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import co.wiki.project.grupo_1_2.wiki.entity.Application;
+
+public interface ApplicationRepository extends CrudRepository<Application, Long> {
+
+}
+
+
