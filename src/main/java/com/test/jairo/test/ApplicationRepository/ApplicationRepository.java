e45fb482509bccc54b62873cd2137093098c91a9
amers_gos <juandiegopala_@outlook.es>
2024-02-18 13:39:22 -0500

    delete project

--- src/main/java/com/test/jairo/test/ApplicationRepository/ApplicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test.jairo.test.ApplicationRepository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.test.jairo.test.entity.Application;
-
-public interface ApplicationRepository extends CrudRepository<Application, Long> {
-
-}
