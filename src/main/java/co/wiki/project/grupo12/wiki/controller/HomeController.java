287a5f7effc25392bfcf505553bc6805b22be9c9
amers_gos <juandiegopala_@outlook.es>
2024-02-29 01:31:50 -0500

    c

--- src/main/java/co/wiki/project/grupo12/wiki/controller/HomeController.java
+++ src/main/java/co/wiki/project/grupo12/wiki/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/grupo12/plantilla")
+@RequestMapping("/grupo12")
 public class HomeController {
 
     @GetMapping("/Home")
