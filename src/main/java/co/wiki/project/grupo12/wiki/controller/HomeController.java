2dee250a0d97089591ae2e2084420c1ec502d16e
amers_gos <juandiegopala_@outlook.es>
2024-02-29 01:35:04 -0500

    q

--- src/main/java/co/wiki/project/grupo12/wiki/controller/HomeController.java
+++ src/main/java/co/wiki/project/grupo12/wiki/controller/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/grupo12")
@@ -33,4 +35,9 @@ public class HomeController {
     public String RequerimientosR() {
         return "Requerimientos";
     }
+    @GetMapping("/DescipcionProyecto")
+    public String DescipcionProyecto() {
+        return "DescipcionProyecto";
+    }
+    
 }
